ac9ba1b9fb4b078a18116bcd145bcc6343a9a7f0
Gabriel Chirico <97834322+GaboChirico@users.noreply.github.com>
2022-09-24 01:06:08 +0200

    Rebase models, functions and folder structure

--- models.py
+++ models.py
@@ -1,55 +1,82 @@
-import dataclasses
+from spoty import time_format
 
 
-@dataclasses.dataclass
 class Meta:
-    name: str
-    album: str
-    artists: str
-    release_date: str
-    duration_ms: float
-    popularity: int
+
+    def __init__(self, meta_data):
+        self.name = meta_data['name']
+        self.album = meta_data['album']['name']
+        self.artists = meta_data['album']['artists'][0]['name']
+        self.release_date = meta_data['album']['release_date']
+        self.duration_ms = time_format(meta_data['duration_ms'])
+        self.popularity = meta_data['popularity']
 
 
-@dataclasses.dataclass
 class Features:
-    danceability: float
-    acousticness: float
-    energy: float
-    instrumentalness: float
-    liveness: float
-    loudness: float
-    speechiness: float
-    key: int
-    mode: int
-    valence: float
-    tempo: float
-    time_signature: int
+
+    def __init__(self, feature_data):
+        self.danceability = feature_data[0]['danceability']
+        self.acousticness = feature_data[0]['acousticness']
+        self.energy = feature_data[0]['energy']
+        self.instrumentalness = feature_data[0]['instrumentalness']
+        self.liveness = feature_data[0]['liveness']
+        self.loudness = feature_data[0]['loudness']
+        self.speechiness = feature_data[0]['speechiness']
+        self.key = feature_data[0]['key']
+        self.mode = feature_data[0]['mode']
+        self.valence = feature_data[0]['valence']
+        self.tempo = feature_data[0]['tempo']
+        self.time_signature = feature_data[0]['time_signature']
 
 
-@dataclasses.dataclass
 class Track:
-    meta: Meta
-    features: Features
+
+    def __init__(self, id: str, meta: Meta, features: Features):
+        self.id = id
+        self.meta = meta
+        self.features = features
+
+    def __str__(self) -> str:
+        return f"{self.meta.name}"
 
 
 class Album:
-    def __init__(self, album_id: str):
-        pass
 
-    def __repr__(self):
-        return f'Album({self.album_id})'
+    def __init__(self, id: str, tracks: list[Track], album_data):
+        self.id = id
+        self.name = album_data['name']
+        self.artist = album_data['artists'][0]['name']
+        self.tracks = tracks
+        self.total_tracks = album_data['total_tracks']
+        self.total_duration = time_format(
+            sum([x['duration_ms'] for x in album_data['tracks']['items']]))
+        self.release_date = album_data['release_date']
+        self.popularity = album_data['popularity']
+        self.label = album_data['label']
+        self.total_markets = len(album_data['available_markets'])
+        self.image = album_data['images'][1]['url']
 
     def __str__(self) -> str:
-        pass
+        return f"Album: {self.name} by {self.artist}"
 
 
 class Playlist:
-    def __init__(self, playlist_id: str):
-        pass
 
-    def __repr__(self):
-        return f'Playlist({self.playlist_id})'
+    def __init__(self, id: str, tracks: list[Track], playlist_data):
+        self.id = id
+        self.name = playlist_data['name']
+        self.tracks = tracks
+        self.followers = playlist_data['followers']['total']
+        self.total_duration = time_format(
+            sum([x['track']['duration_ms'] for x in playlist_data['tracks']['items']]))
+        self.average_popularity = sum(
+            [x['track']['popularity'] for x in playlist_data['tracks']['items']]) / playlist_data['tracks']['total']
+        self.owner = playlist_data['owner']['display_name']
+        self.public = (lambda x: 'Yes' if x else 'No')(playlist_data['public'])
+        self.collaborative = (lambda x: 'Yes' if x else 'No')(
+            playlist_data['collaborative'])
+        self.description = playlist_data['description']
+        self.image = playlist_data['images'][0]['url']
 
     def __str__(self) -> str:
-        pass
+        return f"Playlist: {self.name} by {self.owner}"
--- spoty.py
+++ spoty.py
@@ -1,16 +1,17 @@
 import os
 import logging
+from models import Track, Meta, Features, Album, Playlist
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.cache_handler import CacheFileHandler
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-cache_path = BASE_DIR + '/.cache'
+cache_path = BASE_DIR + 'tmp/.cache'
 cache_handler = CacheFileHandler(cache_path=cache_path)\
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='spotipy.log',
+logging.basicConfig(filename='tmp/spotipy.log',
                     encoding='utf-8', level=logging.DEBUG)
 logging.basicConfig(format='%(asctime)s %(message)s')
 logger.info(cache_handler.get_cached_token())
@@ -22,11 +23,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ.
                                                            client_secret=os.environ.get(
                                                                'SPOTIPY_CLIENT_SECRET'),
                                                            cache_handler=cache_handler))
-
-meta = ['name', 'album', 'artists',
-        'release_date', 'duration_ms', 'popularity']
-features = ['danceability', 'acousticness', 'energy', 'instrumentalness', 'liveness',
-            'loudness', 'speechiness', 'key', 'mode', 'valence', 'tempo', 'time_signature']
 
 
 def time_format(ms: float) -> str:
@@ -46,73 +42,20 @@ def time_format(ms: float) -> str:
         return "{:02}:{:02}".format(int((ms / 1000.0 / 60) % 60), int(ms / 1000.0 % 60))
 
 
-def album_get_tracks_data(album_id: str):
+def album_get_data(album_id: str):
     idList = [item['id'] for item in sp.album(album_id)['tracks']['items']]
-    return [get_track_data(idList[item]) for item in range(len(idList))]
+    return Album(id=album_id, 
+                 tracks=[get_track_data(idList[item]) for item in range(len(idList))], 
+                 album_data=sp.album(album_id))
 
 
 def playlist_get_tracks_data(playlist_id: str):
     idList = [item['track']['id']
               for item in sp.playlist(playlist_id)['tracks']['items']]
-    return [get_track_data(idList[item]) for item in range(len(idList))]
+    return Playlist(id=playlist_id, 
+                    tracks=[get_track_data(idList[item]) for item in range(len(idList))],
+                    playlist_data=sp.playlist(playlist_id))
 
 
 def get_track_data(id: str):
-    # meta
-    get_meta = sp.track(id)
-    name = get_meta['name']
-    album = get_meta['album']['name']
-    artist = get_meta['album']['artists'][0]['name']
-    release_date = get_meta['album']['release_date']
-    length = time_format(int(get_meta['duration_ms']))
-    popularity = get_meta['popularity']
-
-    # features
-    get_features = sp.audio_features(id)
-    danceability = get_features[0]['danceability']
-    acousticness = get_features[0]['acousticness']
-    energy = get_features[0]['energy']
-    instrumentalness = get_features[0]['instrumentalness']
-    liveness = get_features[0]['liveness']
-    loudness = get_features[0]['loudness']
-    speechiness = get_features[0]['speechiness']
-    key = get_features[0]['key']
-    mode = get_features[0]['mode']
-    valence = get_features[0]['valence']
-    tempo = get_features[0]['tempo']
-    time_signature = get_features[0]['time_signature']
-
-    return [name, album, artist, release_date, length, popularity, danceability, acousticness, energy,
-            instrumentalness, liveness, loudness, speechiness, key, mode, valence, tempo, time_signature]
-
-
-def get_album_data(album_id: str):
-    # albumDict = {
-    #     'Album': get_data['name'],
-    #     'Artist': get_data['artists'][0]['name'],
-    #     'Tracks': get_data['total_tracks'],
-    #     'Total Duration': time_format(sum([x['duration_ms'] for x in get_data['tracks']['items']])),
-    #     'Release': get_data['release_date'],
-    #     'Popularity': get_data['popularity'],
-    #     'Label': get_data['label'],
-    #     'Markets': len(get_data['available_markets']),
-    #     'Album Cover': get_data['images'][1]['url'],
-    # }
-    return sp.album(album_id)
-
-
-def get_playlist_data(playlist_id: str):
-    get_data = sp.playlist(playlist_id)
-    # playlistDict = {
-    #     'Playlist': get_data['name'],
-    #     'Tracks': get_data['tracks']['total'],
-    #     'Followers': get_data['followers']['total'],
-    #     'Total Duration': time_format(sum([x['track']['duration_ms'] for x in get_data['tracks']['items']])),
-    #     'Average Popularity': sum([x['track']['popularity'] for x in get_data['tracks']['items']]) / get_data['tracks']['total'],
-    #     'Owner': get_data['owner']['display_name'],
-    #     'Public': (lambda x: 'Yes' if x else 'No')(get_data['public']),
-    #     'Collaborative': (lambda x: 'Yes' if x else 'No')(get_data['collaborative']),
-    #     'Description': get_data['description'],
-    #     'Playlist Cover': get_data['images'][0]['url']
-    # }
-    return sp.playlist(playlist_id)
+    return Track(id=id, meta=Meta(sp.track(id)), features=Features(sp.audio_features(id)))
