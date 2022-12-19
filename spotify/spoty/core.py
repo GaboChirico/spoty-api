e570e5b9b5fbdb3aa399330dfd5b92559af7f2d6
Gabriel Chirico <gacn40@gmail.com>
2022-12-19 10:56:28 +0000

    black format

--- spotify/spoty/core.py
+++ spotify/spoty/core.py
@@ -11,20 +11,19 @@ class Spoty:
     @track_time
     def __call__(self):
         self._valid_query()
-        if self.query.type == 'track':
+        if self.query.type == "track":
             return self.get_track(self.query.query)
-        elif self.query.type == 'album':
+        elif self.query.type == "album":
             return self.get_album(self.query.query)
-        elif self.query.type == 'playlist':
+        elif self.query.type == "playlist":
             return self.get_playlist(self.query.query)
         else:
-            raise ValueError('Invalid type')
+            raise ValueError("Invalid type")
 
     def _valid_query(self):
         try:
-            self.client.search(q=self.query.query,
-                               type=self.query.type, limit=1)
-            LOGGER.info(f'Query: {self.query.query} is valid')
+            self.client.search(q=self.query.query, type=self.query.type, limit=1)
+            LOGGER.info(f"Query: {self.query.query} is valid")
         except Exception as e:
             LOGGER.error(e)
 
@@ -32,39 +31,54 @@ class Spoty:
         """
         Get album tracks and features.
         """
-        return Album(id=album_id,
-                     tracks=[self.get_track(self.get_ids(album_id, type='album')[item])
-                             for item in range(len(self.get_ids(album_id, type='album')))],
-                     album_data=self.client.album(album_id))
+        return Album(
+            id=album_id,
+            tracks=[
+                self.get_track(self.get_ids(album_id, type="album")[item])
+                for item in range(len(self.get_ids(album_id, type="album")))
+            ],
+            album_data=self.client.album(album_id),
+        )
 
     def get_playlist(self, playlist_id: str) -> Playlist:
         """
         Get playlist tracks and features.
         """
-        return Playlist(id=playlist_id,
-                        tracks=[self.get_track(self.get_ids(playlist_id, type='playlist')[item])
-                                for item in range(len(self.get_ids(playlist_id, type='playlist')))],
-                        playlist_data=self.client.playlist(playlist_id))
+        return Playlist(
+            id=playlist_id,
+            tracks=[
+                self.get_track(self.get_ids(playlist_id, type="playlist")[item])
+                for item in range(len(self.get_ids(playlist_id, type="playlist")))
+            ],
+            playlist_data=self.client.playlist(playlist_id),
+        )
 
     def get_ids(self, id, type: str) -> list[str]:
         """
         Get track IDs from album or playlist.
         """
-        if type == 'album':
-            return [track['id'] for track in self.client.album_tracks(id)['items']]
-        elif type == 'playlist':
-            return [item['track']['id'] for item in self.client.playlist(id)['tracks']['items']]
+        if type == "album":
+            return [track["id"] for track in self.client.album_tracks(id)["items"]]
+        elif type == "playlist":
+            return [
+                item["track"]["id"]
+                for item in self.client.playlist(id)["tracks"]["items"]
+            ]
         else:
-            raise ValueError('Invalid type')
+            raise ValueError("Invalid type")
 
     def get_track(self, id: str) -> Track:
         """
         Get track meta and features.
         """
-        return Track(id=id, meta=Meta(self.client.track(id)), features=Features(self.client.audio_features(id)))
+        return Track(
+            id=id,
+            meta=Meta(self.client.track(id)),
+            features=Features(self.client.audio_features(id)),
+        )
 
     def __str__(self) -> str:
-        return f'{self.query.type}'
+        return f"{self.query.type}"
 
     def __repr__(self) -> str:
-        return f'{self.query.type}'
+        return f"{self.query.type}"
--- spotify/spoty/models.py
+++ spotify/spoty/models.py
@@ -1,82 +1,86 @@
 from dataclasses import dataclass
 from .utils import time_format, pitch_class_notation
 
+
 @dataclass
 class Query:
     query: str
     type: str
     limit: int = 50
-    
-class Meta:
 
+
+class Meta:
     def __init__(self, meta_data):
-        self.name = meta_data['name']
-        self.album = meta_data['album']['name']
-        self.artists = meta_data['album']['artists'][0]['name']
-        self.release_date = meta_data['album']['release_date']
-        self.duration_ms = time_format(meta_data['duration_ms'])
-        self.popularity = meta_data['popularity']
+        self.name = meta_data["name"]
+        self.album = meta_data["album"]["name"]
+        self.artists = meta_data["album"]["artists"][0]["name"]
+        self.release_date = meta_data["album"]["release_date"]
+        self.duration_ms = time_format(meta_data["duration_ms"])
+        self.popularity = meta_data["popularity"]
 
 
 class AlbumMeta:
-
     def __init__(self, meta_data):
-        self.name = meta_data['name']
-        self.artists = meta_data['artists'][0]['name']
-        self.release_date = meta_data['release_date']
-        self.total_tracks = meta_data['total_tracks']
-        self.popularity = meta_data['popularity']
+        self.name = meta_data["name"]
+        self.artists = meta_data["artists"][0]["name"]
+        self.release_date = meta_data["release_date"]
+        self.total_tracks = meta_data["total_tracks"]
+        self.popularity = meta_data["popularity"]
         self.total_duration = time_format(
-            sum([x['duration_ms'] for x in meta_data['tracks']['items']]))
-        self.label = meta_data['label']
-        self.total_markets = len(meta_data['available_markets'])
-        self.image = meta_data['images'][1]['url']
-        
+            sum([x["duration_ms"] for x in meta_data["tracks"]["items"]])
+        )
+        self.label = meta_data["label"]
+        self.total_markets = len(meta_data["available_markets"])
+        self.image = meta_data["images"][1]["url"]
+
     def __str__(self):
         for key, value in self.__dict__.items():
-            print(f'{key}: {value}')
-        return ''
+            print(f"{key}: {value}")
+        return ""
 
 
 class PlaylistMeta:
-
     def __init__(self, meta_data):
-        self.name = meta_data['name']
-        self.followers = meta_data['followers']['total']
+        self.name = meta_data["name"]
+        self.followers = meta_data["followers"]["total"]
         self.total_duration = time_format(
-            sum([x['track']['duration_ms'] for x in meta_data['tracks']['items']]))
-        self.average_popularity = sum(
-            [x['track']['popularity'] for x in meta_data['tracks']['items']]) / meta_data['tracks']['total']
-        self.owner = meta_data['owner']['display_name']
-        self.public = (lambda x: 'Yes' if x else 'No')(meta_data['public'])
-        self.collaborative = (lambda x: 'Yes' if x else 'No')(
-            meta_data['collaborative'])
-        self.description = meta_data['description']
-        self.image = meta_data['images'][0]['url']
+            sum([x["track"]["duration_ms"] for x in meta_data["tracks"]["items"]])
+        )
+        self.average_popularity = (
+            sum([x["track"]["popularity"] for x in meta_data["tracks"]["items"]])
+            / meta_data["tracks"]["total"]
+        )
+        self.owner = meta_data["owner"]["display_name"]
+        self.public = (lambda x: "Yes" if x else "No")(meta_data["public"])
+        self.collaborative = (lambda x: "Yes" if x else "No")(
+            meta_data["collaborative"]
+        )
+        self.description = meta_data["description"]
+        self.image = meta_data["images"][0]["url"]
 
 
 class Features:
-
     def __init__(self, feature_data):
-        self.danceability = feature_data[0]['danceability']
-        self.acousticness = feature_data[0]['acousticness']
-        self.energy = feature_data[0]['energy']
-        self.instrumentalness = feature_data[0]['instrumentalness']
-        self.liveness = feature_data[0]['liveness']
-        self.loudness = feature_data[0]['loudness']
-        self.speechiness = feature_data[0]['speechiness']
-        self.key = (lambda x: pitch_class_notation[str(x)])(feature_data[0]['key'])
-        self.mode = (lambda x: 'Major' if x=="1" else 'Minor')(feature_data[0]['mode'])
-        self.valence = feature_data[0]['valence']
-        self.tempo = feature_data[0]['tempo']
-        self.time_signature = feature_data[0]['time_signature']
-        self.valence = feature_data[0]['valence']
-        self.tempo = feature_data[0]['tempo']
-        self.time_signature = feature_data[0]['time_signature']
+        self.danceability = feature_data[0]["danceability"]
+        self.acousticness = feature_data[0]["acousticness"]
+        self.energy = feature_data[0]["energy"]
+        self.instrumentalness = feature_data[0]["instrumentalness"]
+        self.liveness = feature_data[0]["liveness"]
+        self.loudness = feature_data[0]["loudness"]
+        self.speechiness = feature_data[0]["speechiness"]
+        self.key = (lambda x: pitch_class_notation[str(x)])(feature_data[0]["key"])
+        self.mode = (lambda x: "Major" if x == "1" else "Minor")(
+            feature_data[0]["mode"]
+        )
+        self.valence = feature_data[0]["valence"]
+        self.tempo = feature_data[0]["tempo"]
+        self.time_signature = feature_data[0]["time_signature"]
+        self.valence = feature_data[0]["valence"]
+        self.tempo = feature_data[0]["tempo"]
+        self.time_signature = feature_data[0]["time_signature"]
 
 
 class Track:
-
     def __init__(self, id: str, meta: Meta, features: Features):
         self.id = id
         for k, v in meta.__dict__.items():
@@ -86,7 +90,6 @@ class Track:
 
 
 class Album:
-
     def __init__(self, id: str, tracks: list[Track], album_data):
         self.id = id
         self.tracks = tracks
@@ -97,7 +100,6 @@ class Album:
 
 
 class Playlist:
-
     def __init__(self, id: str, tracks: list[Track], playlist_data):
         self.id = id
         self.tracks = tracks
